c2d4f11b9f4fa4248a3779587518c00749aac014
Kevin Inahuazo <inahuazokevin05@gmail.com>
2025-12-07 00:54:18 -0500

    altered beginning page

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Kevin Inahuazo's Portfolio
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`Welcome! My name is Kevin Inahuazo and I am a Junior Statistics major 
+        with a minor in Computer Science studying at Villanova University. I have 
+        aspirations to utilize data to drive decision-making and create impactful solutions.
+        My interests lie within data analysis, operations research, and data science. I 
+        look forward to connecting!`}
       </p>
       <div className="my-8">
         <BlogPosts />
